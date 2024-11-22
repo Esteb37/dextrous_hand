5cba0552c3a5da700e4f244226c059d1b0977622
Esteb37 <esteban37padilla@gmail.com>
2024-11-22 17:31:35 +0100

    Added new URDF

--- launch/rosbag_simulation.py
+++ launch/rosbag_simulation.py
@@ -5,6 +5,19 @@ from launch import LaunchDescription
 from dextrous_hand.utils.utils import parent_dir, DexNode
 
 def generate_launch_description():
+
+    p4_urdf = os.path.join(
+                    parent_dir(),
+                    "data",
+                    "assets",
+                    "urdf",
+                    "hh_hand.urdf",
+                )
+
+
+    with open(p4_urdf, 'r') as infp:
+        p4_robot_desc = infp.read()
+
     return LaunchDescription([
         DexNode("mujoco_node"),
 
@@ -23,6 +36,27 @@ def generate_launch_description():
             ]
         ),
 
+        DexNode("visualize_joints_node",
+            parameters=[
+                {"scheme_path": os.path.join(
+                    parent_dir(),
+                    "data",
+                    "assets",
+                    "scheme_hh.yaml",
+                )}
+            ]
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace='hand',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': p4_robot_desc,}],
+            remappings=[('/joint_states', '/hand/joint_states')]
+        ),
+
+
         Node(package = "rviz2",
              executable="rviz2",
              name="rviz2",
@@ -30,7 +64,7 @@ def generate_launch_description():
                         os.path.join(parent_dir(),
                                      "data",
                                      "rviz",
-                                     "mano_points.rviz")
+                                     "urdf.rviz")
                         ],
             ),
     ])
