dfb45292c4bf256afa26eab543c84677e3b4c3c4
Esteb37 <esteban37padilla@gmail.com>
2024-12-14 11:26:13 +0100

    Yolo decider done

--- src/yolo_node.py
+++ src/yolo_node.py
@@ -7,6 +7,7 @@ import cv2
 
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 
 bridge = CvBridge()
 
@@ -22,29 +23,84 @@ class YOLONode(Node):
 
         self.front_pub = self.create_publisher(Image, "/yolo_front_view", 10)
         self.side_pub = self.create_publisher(Image, "/yolo_side_view", 10)
+        self.decision_pub = self.create_publisher(String, "/yolo_decision", 10)
+
+        self.front_decision = "unsure"
+        self.side_decision = "unsure"
+
+        self.decide_looper = self.create_timer(0.1, self.decide_loop)
+
+        self.side_init = False
+        self.front_init = False
+
+        self.start_time = float(self.get_clock().now().nanoseconds)
+        self.decision = "unsure"
+
+        self.get_logger().warn("Yolo node started")
 
 
     def front_callback(self, msg):
-        image = self.process_image(msg)
+        self.front_init = True
+        if self.decision != "unsure":
+            return
+
+        side, image = self.process_image(msg, "front")
+        self.front_decision = side
         if image is not None:
             image = bridge.cv2_to_imgmsg(image, "bgr8")
             self.front_pub.publish(image)
 
     def side_callback(self, msg):
-        image = self.process_image(msg)
+        self.side_init = True
+        if self.decision != "unsure":
+            return
+
+        side, image = self.process_image(msg, "side")
+        self.side_decision = side
         if image is not None:
             image = bridge.cv2_to_imgmsg(image, "bgr8")
             self.side_pub.publish(image)
 
-    def process_image(self, image):
+    def decide(self):
+        if self.front_decision != self.side_decision:
+            return "unsure"
+
+        return self.front_decision
+
+    def decide_loop(self):
+        if self.decision != "unsure":
+            msg = String()
+            msg.data = self.decision
+            self.decision_pub.publish(msg)
+            return
+
+        if not self.front_init or not self.side_init:
+            self.start_time = float(self.get_clock().now().nanoseconds)
+            return
+
+        stop_time = float(self.get_clock().now().nanoseconds)
+
+        if stop_time - self.start_time > 2e9:
+            self.get_logger().warn("YOLO unsure")
+            msg = String()
+            msg.data = "unsure"
+            self.decision_pub.publish(msg)
+
+        self.decision = self.decide()
+
+        if self.decision == "unsure":
+            self.get_logger().info("YOLO unsure for: " + str((stop_time - self.start_time)/1e9) + " seconds")
+
+        else:
+            msg = String()
+            msg.data = self.decision
+            self.decision_pub.publish(msg)
+
+    def process_image(self, image, camera, visualize = False):
 
         image = bridge.imgmsg_to_cv2(image)
 
         results = self.model(image, imgsz=224)
-
-        if len(results) == 0:
-            return None
-
 
         best_cube = None
         best_cube_conf = 0
@@ -55,11 +111,13 @@ class YOLONode(Node):
         best_blue = None
         best_blue_conf = 0
 
+        side = "unsure"
+
         for result in results:
             for box in result.boxes:
 
-                array = box.data.cpu().numpy()
-                x1, y1, x2, y2, conf, clas = array[0]
+                array = box.data.cpu().numpy()[0]
+                x1, y1, x2, y2, conf, clas = array
 
                 if clas <= 5:
                     if conf > best_cube_conf:
@@ -82,41 +140,59 @@ class YOLONode(Node):
                         best_red_conf = conf
 
             if best_cube is not None:
-                x1, y1, x2, y2, conf, clas = best_cube[0]
-                if clas in [0, 3]:
+                x1, y1, x2, y2, conf, clas = best_cube
+                if clas in [0, 3] and best_blue is not None:
                     corr_tray = best_blue
-                if clas in [1, 4]:
+                if clas in [1, 4] and best_yellow is not None:
                     corr_tray = best_yellow
-                if clas in [2, 5]:
+                if clas in [2, 5] and best_red is not None:
                     corr_tray = best_red
 
-            trays = []
-            if best_blue is not None:
-                trays.append(best_blue[0])
-            if best_yellow is not None:
-                trays.append(best_yellow[0])
-            if best_red is not None:
-                trays.append(best_red[0])
+                if corr_tray is None:
+                    return
 
+                trays = []
+                if best_blue is not None:
+                    trays.append(best_blue)
+                if best_yellow is not None:
+                    trays.append(best_yellow)
+                if best_red is not None:
+                    trays.append(best_red)
 
-            # Sort from left to right
-            trays.sort(key=lambda x: x[0])
+                if camera == "front":
+                    trays.sort(key=lambda x: x[0])
+                else:
+                    trays.sort(key=lambda x: x[0], reverse=True)
 
-            for rect in [best_cube, corr_tray]:
-                if rect is None:
-                    continue
+                side = "left"
 
-                x1, y1, x2, y2, conf, clas = rect[0]
+                if trays[2][5] == corr_tray[5]:
+                    side = "right"
 
-                x1 = int(x1)
-                y1 = int(y1)
-                x2 = int(x2)
-                y2 = int(y2)
-                clas = int(clas)
-                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 1)
-                cv2.putText(image, classes[clas], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                if trays[1][5] == corr_tray[5]:
+                    side = "center"
 
-        return image
+                if visualize:
+                    for rect in [best_cube, corr_tray]:
+                        if rect is None:
+                            continue
+
+                        x1, y1, x2, y2, conf, clas = rect
+
+                        x1 = int(x1)
+                        y1 = int(y1)
+                        x2 = int(x2)
+                        y2 = int(y2)
+                        clas = int(clas)
+                        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                        cv2.putText(image, classes[clas], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+        if visualize:
+            cv2.putText(image, side, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            return side, image
+
+        else:
+            return side, None
 
 
 def main(args=None):
